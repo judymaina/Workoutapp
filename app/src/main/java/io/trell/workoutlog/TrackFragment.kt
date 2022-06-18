108c05aec90fb0984dc7424f0781999b8568336f
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-06-18 09:00:00 +0300

    added fragments

--- app/src/main/java/io/trell/workoutlog/TrackFragment.kt
+++ app/src/main/java/io/trell/workoutlog/TrackFragment.kt
@@ -6,14 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class PlanFragment : Fragment() {
+
+class TrackFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan, container, false)
+        return inflater.inflate(R.layout.fragment_track, container, false)
     }
 
 
